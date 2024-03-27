60bce4b72086648fe96d2ce605ac1450e8a0fab1
shahsarefin <shahsarefin@gmail.com>
2024-03-27 15:23:08 -0500

    routes updated for cart admin users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
 
+ 
+  devise_for :users, path: 'auth'
 
   get '/about', to: 'static#show', page_name: 'about'
   get '/contact', to: 'static#show', page_name: 'contact'
@@ -10,9 +11,24 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'products', to: 'products#index'
   resources :products, only: [:show]
+
+  resources :categories, only: [:index, :show]
+
+  resource :cart, only: [:show] do
+    post 'add_to_cart/:product_id', to: 'carts#add_to_cart', as: :add_to_cart
+    post 'update_cart_item/:product_id', to: 'carts#update_cart_item', as: 'update_item'
+    delete 'remove_from_cart/:product_id', to: 'carts#remove_from_cart', as: 'remove_from_cart'
+  end
+
+  resources :orders, only: [:new, :create, :show]
+  resource :checkout, only: [:show, :create]
+
   
-resources :categories, only: [:index, :show]
+  delete '/users/sign_out', to: 'devise/sessions#destroy', as: :user_sign_out
 
-resources :carts, only: [:index, :create, :update, :destroy]
+  get 'profile', to: 'users#show', as: :user_profile
 
-end 
+  
+post 'orders/confirm', to: 'orders#confirm', as: 'confirm_order'
+  
+end
